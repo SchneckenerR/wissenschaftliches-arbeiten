fd1ea3b09e57a671e471d8365d3def64362490c5
SchneckenerR <ruth@schneckener.de>
2023-02-01 11:50:02 +0100

    Update Hilfsfunktionen.R

--- Hilfsfunktionen.R
+++ Hilfsfunktionen.R
@@ -3,18 +3,20 @@
 ## Berechnung der Schiefe nach drei Typen (Pearson, Yule-Pearson und Yule)
 
 schiefe <- function(x){
-    res <- (mean(x) - quantile(x, 0.5)/sd(x)
+    res <- (mean(x) - median(x))/sd(x)
     return(res)
     }
 
 ## Berechnung der Woelbung:
 
 woelbung <- function(x){
+  res <- 0
+  n <- length(x)
   for(i in 1:n){
     res <- (x[i]-mean(x))^4 + res
   }
   res <- res/(sd(x)^4)
-  res <- res/n-1
+  res <- res/(n-1)
   res <- res - 3
   return(res)
 }
